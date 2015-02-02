f2230705c6fd606a9d4602b1f42cd563d0f5dd43
Michael Fasani <michaelfasani@gmail.com>
2015-02-02 22:09:38 +0100

    Fixing track loops

--- app.js
+++ app.js
@@ -7,82 +7,106 @@ var tracks = require('./tracks');
 var spotify = new nodeSpotifyWebHelper.SpotifyWebHelper();
 
 var _skipCount = 0;
-
+var _myNextTrackTimer = 0;
 // Calling a new client..
 var client = new irc.client({
-    options: {
-        debug: true,
-        debugIgnore: ['ping', 'chat', 'action'],
-        logging: false,
-        tc: 3
-    },
-    identity: {
-        username: '',
-        password: ''
-    },
-    channels: ['fivemandown']
+  options: {
+    debug: true,
+    debugIgnore: ['ping', 'chat', 'action'],
+    logging: false,
+    tc: 3
+  },
+  identity: {
+    username: 'SpottyBot',
+    password: 'oauth:y13g9wu7jilquha4489dtde0fxvuyv'
+  },
+  channels: ['fivemandown']
 });
 
 // Connect the client to server..
 client.connect();
 
 // Your events..
-client.addListener('chat', function(channel, user, message) {
+client.addListener('chat', function (channel, user, message) {
 
-    if (message.indexOf('!song') === 0) {
-        // Get the name of the song which is currently playing
-        whatSong(channel, user, message);
-    }
+  if (message.indexOf('!song') === 0) {
+    // Get the name of the song which is currently playing
+    whatSong(channel, user, message);
+  }
 
-    if (message.indexOf('!skip') === 0) {
-        // Skip track
-        skip(channel, user, message);
-    }
+  if (message.indexOf('!skip') === 0) {
+    // Skip track
+    skip(channel, user, message);
+  }
 
 });
 
 function whatSong(channel, user, message) {
-    spotify.getStatus(function(err, res) {
-        if (err) {
-            return console.error(err);
-        }
+  spotify.getStatus(function (err, res) {
+    if (err) {
+      return console.error(err);
+    }
 
-        var message = 'Currently playing: ' + res.track.artist_resource.name + ' - ' + res.track.track_resource.name + ' (' + res.track.track_resource.location.og + ')';
-        client.say(channel, message);
-    });
+    var message = 'Currently playing: ' + res.track.artist_resource.name + ' - ' + res.track.track_resource.name + ' (' + res.track.track_resource.location.og + ')';
+    client.say(channel, message);
+  });
 }
 
 function getTrack() {
-    var trackNumber = Math.floor(Math.random() * tracks.length + 1);
-    var track = tracks.allTracks[trackNumber].uri;
-    return track;
+  var myTracks = tracks.allTracks();
+  var trackNumber = Math.floor(Math.random() * myTracks.length + 1);
+
+  return myTracks[trackNumber].uri;
+}
+
+function play(track) {
+
+  spotify.play(track, function (err, res) {
+
+    if (err) {
+      console.info(err);
+      return console.error(err);
+    }
+
+    if (res) {
+
+      clearTimeout(_myNextTrackTimer);
+      var timeBeforeNextTrack = res.track.length * 1000;
+
+      _myNextTrackTimer = setTimeout(function () {
+        var newTrack = getTrack();
+
+        console.log(newTrack);
+        console.log(timeBeforeNextTrack);
+
+        play(newTrack);
+
+      }, timeBeforeNextTrack);
+
+    }
+
+  });
+
 }
 
 function skip(channel, user, message) {
 
-    if (_skipCount === 4) {
+  if (_skipCount === 4) {
 
-        _skipCount = _skipCount + 1;
-        var message = _skipCount + " / 5 votes needed to skip this track! - Track Skipped!";
-        client.say(channel, message);
+    _skipCount = _skipCount + 1;
+    var message = _skipCount + " / 5 votes needed to skip this track! - Track Skipped!";
+    client.say(channel, message);
 
-        _skipCount = 0
+    _skipCount = 0
 
-        var track = getTrack();
+    var track = getTrack();
 
-        spotify.play(track, function(err, res) {
+    play(track);
 
-            if (err) {
-                console.info(err);
-                return console.error(err);
-            }
-
-        });
-
-    } else {
-        _skipCount = _skipCount + 1;
-        var message = _skipCount + " / 5 votes needed to skip this track!";
-        client.say(channel, message);
-    }
+  } else {
+    _skipCount = _skipCount + 1;
+    var message = _skipCount + " / 5 votes needed to skip this track!";
+    client.say(channel, message);
+  }
 
 }
