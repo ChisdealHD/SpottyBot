d66ec1380fac0c05d6fb23cbebe650f5da06d596
Michael Fasani <michaelfasani@gmail.com>
2015-02-08 21:25:00 +0100

    Adding local storage and hooking up the GUI

--- settings.js
+++
@@ -1,8 +0,0 @@
-//https://github.com/nwjs/nw.js/wiki/Save-persistent-data-in-app
-
-module.exports = {
-  botUsername: 'SpottyBot',
-  botAuthToken: 'oauth:y13g9wu7jilquha4489dtde0fxvuyv',
-  twitchChannels: ['fivemandown'],
-  skipCount: 3
-};
